575906ad38e78c0bb55368cbb4230d4c7f2be3b7
marcos laina perez <laina855@gmail.com>
2022-10-06 18:06:42 +0200

    init modal

--- src/main.js
+++ src/main.js
@@ -20,3 +20,30 @@ for (let i=0; i<lands.length; i++) {
     path.style.fill='#fff';
   });
 }
+
+
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
