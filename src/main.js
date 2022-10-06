bb526e67a50886c417e80537863a08367fa39605
marcos laina perez <laina855@gmail.com>
2022-10-06 17:20:27 +0200

    hovers

--- src/main.js
+++ src/main.js
@@ -1,58 +1,5 @@
-
-
 let lands = document.querySelectorAll('.lands');
-
-
-
-
-
-function getAverageRGB(imgEl) {
-
-  var blockSize = 5, // only visit every 5 pixels
-    defaultRGB = {r:0,g:0,b:0}, // for non-supporting envs
-    canvas = document.createElement('canvas'),
-    context = canvas.getContext && canvas.getContext('2d'),
-    data, width, height,
-    i = -4,
-    length,
-    rgb = {r:0,g:0,b:0},
-    count = 0;
-
-  if (!context) {
-    return defaultRGB;
-  }
-  height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
-  width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
-
-  context.drawImage(imgEl, 0, 0);
-
-  try {
-    data = context.getImageData(0, 0, width, height);
-  } catch(e) {
-    console.log("err")
-    /* security error, img on diff domain */
-    return defaultRGB;
-  }
-
-
-  length = data.data.length;
-
-  while ( (i += blockSize * 4) < length ) {
-    ++count;
-    rgb.r += data.data[i];
-    rgb.g += data.data[i+1];
-    rgb.b += data.data[i+2];
-  }
-
-  // ~~ used to floor values
-  rgb.r = ~~(rgb.r/count);
-  rgb.g = ~~(rgb.g/count);
-  rgb.b = ~~(rgb.b/count);
-
-  return rgb;
-
-}
-
+let selected = document.querySelector('#selected');
 
 
 for (let i=0; i<lands.length; i++) {
@@ -60,8 +7,16 @@ for (let i=0; i<lands.length; i++) {
   lands[i].addEventListener('mouseover', () => {
     let path = document.querySelector('path#'+lands[i].id);
 
-    let rgb = getAverageRGB(lands[i].childNodes[1]);
-     console.log(rgb);
-  });
+//    let rgb = getAverageRGB(lands[i].childNodes[1]);
+    selected.setHTML(lands[i].id.toString().toUpperCase());
 
+    path.style.fill='#772844';
+   });
+
+  lands[i].addEventListener('mouseout', () => {
+    let path = document.querySelector('path#'+lands[i].id);
+
+    selected.setHTML("");
+    path.style.fill='#fff';
+  });
 }
